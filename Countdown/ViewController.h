20d4b9aa3870db9e048c5568d78fd57bbb390b3f
alessandro-martin <martin.acg@gmail.com>
2014-10-07 09:15:51 +0200

    Initial Commit

---
+++ Countdown/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Countdown
+//
+//  Created by Alessandro on 07/10/14.
+//  Copyright (c) 2014 Alessandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Countdown/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Countdown
+//
+//  Created by Alessandro on 07/10/14.
+//  Copyright (c) 2014 Alessandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
